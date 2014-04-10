ff6c4af13637f344cdbea01d65e61fa159e4630b
RubaXa <ibnRubaXa@gmail.com>
2014-04-10 23:40:18 +0400

    + Home screen

--- tutorial/11-jquery-requirejs-finally/app.js
+++ tutorial/11-jquery-requirejs-finally/app.js
@@ -1,5 +1,44 @@
 (function ($, Pilot) {
+	var App = Pilot.create({
+		el: '#app', // root view
 
-	/* app code */
 
+		// Home screen
+		'/': {
+			// Route id
+			id: 'home',
+
+			/**
+			 * This method should be called before `init`, `routestart` or `routechange`
+			 * @param   {Pilot.Request}  req
+			 * @returns {$.Deferred}
+			 */
+			loadData: function (req) {
+				return $.getJSON('./data/galleries.json');
+			},
+
+			/**
+			 * Will be called once at the time of initialization of the route
+			 */
+			init: function () {
+				// Retrieve a loaded data
+				var galleries = this.getLoadedData();
+
+				// Set a header screen
+				this.$('.js-title').text('List of art movements');
+
+				// Build the list
+				this.$('.js-list').append(galleries.map(function (name) {
+					var url = this.getUrl('gallery', { name: name });
+					return '<li class="table-view-cell"><a href="'+url+'">'+name+'</a></li>';
+				}, this));
+			}
+		}
+
+
+	});
+
+
+	// Run app
+	App.start('/');
 })(jQuery, Pilot);
