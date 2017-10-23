d50bcbf03ed318d0b17641b9354a8afff689873e
Marina Titova <marina.titova@corp.mail.ru>
2017-10-23 19:23:11 +0300

    experiment

--- Pilot.js
+++ Pilot.js
@@ -3,12 +3,12 @@
 								var defined = {Emitter: Emitter};
 								var syncDefine = function (name, deps, callback) {
 									var i = deps.length, depName;
-	
+
 									while (i--) {
 										depName = name.split('/').slice(0, -1).join('/');
 										deps[i] = defined[deps[i].replace('./', depName ? depName + '/' : '')];
 									}
-	
+
 									defined[name] = callback.apply(null, deps);
 								};
 								syncDefine.amd = true;
@@ -593,6 +593,10 @@ define('src/loader',['./match'], function (match, Emitter) {
 				_options.processing && (models = _options.processing(req, models));
 
 				return models;
+			}).catch(function (err) {
+				console.error(err);
+
+				throw err;
 			});
 
 			if (_options.persist) {
@@ -736,7 +740,7 @@ define('src/route',[
 				if (rule['default'] != null) {
 					target[name] = rule['default'];
 				}
-				
+
 				return true;
 			}
 
